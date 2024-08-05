71e42d139d8622f00c0cd5d1c27b507c2e5497be
Guille737 <guillermobruna06@gmail.com>
2024-08-05 20:58:07 -0300

    tarea 1

---
+++ calcular xl/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.Write("Ingrese el valor de frecuencia: ")
+        Dim Frecuencia As String
+        Frecuencia = Console.ReadLine
+        Console.Write("Ingrese el valor de inductancia: ")
+        Dim Inductor As String
+        Inductor = Console.ReadLine
+        Dim resultado As String
+        resultado = (2 * 3.1415 * Frecuencia * Inductor)
+        Console.WriteLine("su resultado es: " & resultado)
+    End Sub
+
+End Module
